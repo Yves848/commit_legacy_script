0f5b25a3925ee4422b9449925abea58472157c7b
yves848 <yves.godart@gmail.com>
2025-03-25 20:30:41 +0100

    WIP

--- commit-utils.ps1
+++ commit-utils.ps1
@@ -1,5 +1,31 @@
 $path = (Get-Location).Path
 
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WinAPI {
+    [DllImport("user32.dll")]
+    public static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+}
+"@
+
+function maximize {
+  # 3 = Maximiser la fenêtre
+  $SW_MAXIMIZE = 3
+  $hWnd = [WinAPI]::GetForegroundWindow()
+  [WinAPI]::ShowWindow($hWnd, $SW_MAXIMIZE)
+}
+
+function restore {
+  $SW_RESTORE = 9
+  $hWnd = [WinAPI]::GetForegroundWindow()
+  [WinAPI]::ShowWindow($hWnd, $SW_RESTORE)
+}
+
 <#
   get-version
   read the 4 values of the dproj version
@@ -35,11 +61,12 @@ function set-version {
 
   if (Test-Path -Path $path) {
     [xml]$proj = Get-Content -Path $path
-    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[2]."#text" = $MajorVer
-    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[3]."#text" = $MinorVer
-    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[4]."#text" = $Realease
-    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[5]."#text" = $Build
-    $proj.Save($path)
+    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[2]."#text" = $version.MajorVer
+    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[3]."#text" = $version.MinorVer
+    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[4]."#text" = $version.Realease
+    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[5]."#text" = $version.Build
+    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfoKeys.VersionInfoKeys[2]."#text" = "$($version.MajorVer).$($version.MinorVer).$($version.Realease).$($version.Build)"
+    $proj.OuterXml | Out-File $path
   }
 }
 
@@ -59,6 +86,7 @@ function Get-Dproj {
   param (
     [string]$loc
   )
+  
   $list = Get-ChildItem -Path . -Recurse -Filter "*.dproj"
   $list = $list | Where-Object { -not $_.FullName.Contains("Composant") }
   $dproj = [System.Collections.Generic.List[PSCustomObject]]@()
@@ -67,5 +95,14 @@ function Get-Dproj {
     $v = "$($version.MajorVer).$($version.MinorVer).$($version.Realease).$($version.Build)"
     $dproj.Add(@{"dproj" = $_.Name; "Version" = $v })
   }
+  
   return $dproj
+}
+
+function display-dproj {
+  maximize
+  Clear-Host  
+  Get-Dproj -loc . | Format-SpectreTable
+  Read-Host
+  restore
 }
