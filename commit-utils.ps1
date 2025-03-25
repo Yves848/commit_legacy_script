b573335fde6860683624d24076be9dbbb8f97608
yves848 <yves.godart@gmail.com>
2025-03-25 15:26:03 +0100

    vérif version ok

--- commit-utils.ps1
+++ commit-utils.ps1
@@ -1,12 +1,25 @@
 $path = (Get-Location).Path
 
 
-get-version {
+function get-version {
   param(
     [string]$path
   )
-    [xml]$proj = Get-content -Path $path
-    $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo
+  [xml]$proj = Get-Content -Path $path
+  try {
+    $MajorVer = $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[2]."#text"
+    $MinorVer = $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[3]."#text"
+    $Realease = $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[4]."#text"
+    $Build = $proj.Project.ProjectExtensions.BorlandProject.'Delphi.Personality'.VersionInfo.VersionInfo[5]."#text"  
+  }
+  catch {
+    $MajorVer = "0"
+    $MinorVer = "0"
+    $Realease = "0"
+    $Build = "0"
+  }
+
+  return @{"MajorVer" = $MajorVer; "MinorVer" = $MinorVer; "Realease" = $Realease; "Build" = $Build }
 }
 
 
@@ -19,9 +32,11 @@ function Get-Dproj {
   $list = $list | Where-Object { -not $_.FullName.Contains("Composant") }
   $dproj = [System.Collections.Generic.List[PSCustomObject]]@()
   $list | ForEach-Object {
-    $verision = get-version -Path $_.FullName
+    $version = get-version -Path $_.FullName
+    $v = "$($version.MajorVer).$($version.MinorVer).$($version.Realease).$($version.Build)"
+    $dproj.Add(@{"dproj" = $_.Name; "Version" = $v})
   }
-  return $list
+  return $dproj
 }
 
-Get-Dproj | select-object -ExpandProperty name | Format-SpectreTable
+Get-Dproj | Select-Object | Format-SpectreTable
